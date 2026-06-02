e29c66ac7ee9f2b896fcf5bf2ba29de193476724
Huk14p <amedeo.catanese.101@gmail.com>
2026-06-02 11:35:21 +0200

    ora il main esegue la HomePage, basterà quindi avviare solo il main

--- Magazzino/src/main/java/org/example/Main.java
+++ Magazzino/src/main/java/org/example/Main.java
@@ -2,8 +2,10 @@ package org.example;
 
 import it.unina.magazzino.boundary.HomePage;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// il main sarà usato esclusivamente per caricare la home page, il resto è demandato
+// a tutte le altri classi
+
+
 public class Main {
     static void main() {
 
