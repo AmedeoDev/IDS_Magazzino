77d6a235493e43314d051d25cd2e2d778b87f2c7
Huk14p <amedeo.catanese.101@gmail.com>
2026-06-04 17:24:50 +0200

    modifiche al DB, ora accedibile anche da remoto

--- Magazzino/src/main/java/it/unina/magazzino/database/DBConnectionManager.java
+++ Magazzino/src/main/java/it/unina/magazzino/database/DBConnectionManager.java
@@ -1,16 +1,41 @@
 package it.unina.magazzino.database;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DBConnectionManager {
 
     private static Connection connection;
 
-    private static final String URL = "jdbc:mysql://localhost:3306/magazzino";
-    private static final String USER = "root";
-    private static final String PSW = "adminSQLP_assword#12";
+    private static final String URL;
+    private static final String USER;
+    private static final String PSW;
+
+
+    static {
+        Properties props = new Properties();
+        try (InputStream in = DBConnectionManager.class.getClassLoader().getResourceAsStream("config.properties")){
+            if(in == null){
+                throw new RuntimeException("File config.properties non caricato correttamente" +
+                        "\nApri il file e carica le tue credenziali");
+            }
+
+            props.load(in);
+        } catch (IOException e){
+            throw new RuntimeException("Errore nel caricamento di config.properties" + e.getMessage());
+        }
+
+        URL = props.getProperty("db.url");
+        USER = props.getProperty("db.user");
+        PSW = props.getProperty("db.password");
+    }
+
+
+
 
     private DBConnectionManager(){}
 
