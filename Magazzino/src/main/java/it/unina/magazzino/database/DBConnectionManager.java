e3723e632eb5942259a153b2ea3b1f791950d07c
fracapoo <redfrancy.04@gmail.com>
2026-06-10 16:26:44 +0200

    suca

--- Magazzino/src/main/java/it/unina/magazzino/database/DBConnectionManager.java
+++ Magazzino/src/main/java/it/unina/magazzino/database/DBConnectionManager.java
@@ -10,7 +10,7 @@ public class DBConnectionManager {
 
     private static final String URL = "jdbc:mysql://localhost:3306/magazzino";
     private static final String USER = "root";
-    private static final String PSW = "adminSQLP_assword#12";
+    private static final String PSW = "m1aSequenza!";
 
     private DBConnectionManager(){}
 
